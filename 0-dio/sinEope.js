ef42f906a378a34cca617092db864ec5f5150e11
BrunoLeonn <brunoleonn123@gmail.com>
2022-11-16 03:09:55 -0300

    updt

---
+++ 0-dio/sinEope.js
@@ -0,0 +1,55 @@
+// Curso DIO: Sintaxe e Operadores JavaScript
+
+/*      Tipos de Operadores:
+    Aritimética
+    Atribuição
+    Comparação
+    Lógica
+    Condicional
+*/
+
+/*  Atribuição:
+    =      Igual (Atribuindo valor)
+    +   =  Equivalente a ( x = x + y )  
+    *   =  Equivalente a ( x = x * y )
+    /   =  Equivalente a ( x = x / y )
+    %   =  Equivalente a ( x = x % y )
+*/
+/*
+    Tipos de operadores:
+    +   Adição
+    -   Subtração
+    /   Divisão
+    %   Módulo
+    **  Exponencial (Elevando numero ao que vem depois desse operador)
+    ++  Adicionar 1
+    --  Subtrair 1
+
+*/
+/*  
+    Operadores de Comparação:
+    ==  Igual a
+    === Mesmo valor e mesmo tipo
+    !=  Diferente
+    !== Valor e tipos diferentes
+    <   Maior que
+    >   Menor que
+    >=  Maior ou igual
+    <=  Menor ou igual
+*/
+/*
+    Lógica:
+    &&  "e"lógico
+    ||  "ou"lógico
+    !   "não"lógico
+ */
+/*
+    Condicional (ternário):
+
+    (condition) ? expression 1 : expression 2
+
+    if (Condition)
+        statement;
+    else
+        statemente;
+*/                                      //expression 1 = True(if) //expression2 = false(else)
--- 0-dom/script.js
+++ 0-dom/script.js
@@ -1,11 +1,12 @@
-function alterar(){
-    let objc = document.getElementById("c1"); // Números recebidos pelo input
-    let objx = document.getElementById("x1"); // Largura
-    let objy = document.getElementById("y1"); // Altura
-    objc.style.left=`${objx.value}px`; // Número objc define Largura para cima(Altera o valor de objx)
-    objc.style.backgroundColor=`${objx.value}`
-    objc.style.backgroundColor=`${objy.value}`
-    objc.style.top=`${objy.value}px`; // Número objc define Altura para cima(Altera o valor de objy)
-    objc.innerHTML = `(${objx.value},${objy.value})` // Os valores no input objc determinam o texto html dentro do bloco objx e objy
-}
+function alterar() {
+        let objc = document.getElementById("c1"); // Números recebidos pelo input
+        let objx = document.getElementById("x1"); // Largura
+        let objy = document.getElementById("y1"); // Altura
+    
+        objc.style.left = `${objx.value}px`; // Número objc define Largura para cima(Altera o valor de objx)
+        objc.style.backgroundColor = `${objx.value}`
+        objc.style.backgroundColor = `${objy.value}`
+        objc.style.top = `${objy.value}px`; // Número objc define Altura para cima(Altera o valor de objy)
+        objc.innerHTML = `(${objx.value},${objy.value})` // Os valores no input objc determinam o texto html dentro do bloco objx e objy
+  }
     //innerHTML é uma propriedade do Element que define ou retorna o conteúdo HTML dentro de um elemento
