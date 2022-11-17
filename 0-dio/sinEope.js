d1365236fc5b49d7f45f466c920e2e7f3704aebb
BrunoLeonn <brunoleonn123@gmail.com>
2022-11-17 01:10:46 -0300

    updt

--- 0-dio/sinEope.js
+++ 0-dio/sinEope.js
@@ -52,4 +52,30 @@
         statement;
     else
         statemente;
-*/                                      //expression 1 = True(if) //expression2 = false(else)
+                   */                   //expression 1 = True(if) //expression2 = false(else)
+    if (time < 10){
+        greeting = "Good morning";
+    }
+    else if (time < 20){
+        greeting = "Good day";
+    }
+    else{
+        greeting = "Good evening";
+    }
+
+    //      OPERADOR TERNÁRIO
+    greeting = (time<10) ? "Good morning":  //time for menor que 10 ?(se for: primeiro valor) : (se não)
+               (time<20) ? "Good day":  //time for menor que 20 ?(se for: segundo valor) : (se não)
+                           "Good evening"   //(terceiro valor)
+
+    //      SWITCH
+    switch (new Date() .getDay()){ //Switch + ( o valor "no caso o dia da semana" )
+        case 6: // 7 porém na computação começa a contar no 0
+            text = "today is Saturday"; //se for 6 é o sabado
+            break; // quebra
+        case 0:
+            text = "today is Sunday"; // se for 0 vai ser domingo
+            break;
+            default: // se não for: esperando o próximo final de semana(segunda a sexta)
+                text = "Looking forward to the Weekend";
+    }
